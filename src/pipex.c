d1a04d8602ea08f772f185b5bc38ba254576cbe7
Luna <luna7111@tuta.io>
2025-01-15 22:17:37 +0100

    Nasty commit :( Added children behaviours (start_behaviour middle_behaviout end_behaviour). Main now opens a fork for each command etc (in short: pipex is working).

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/01/10 16:31:37 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/01/12 14:07:50 by ldel-val          ``                     */
+/*   Updated: 2025/01/15 22:00:03 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,75 @@ char	*command_path(char *name, char **env)
 	return (NULL);
 }
 
+int	start_behaviour(int fd_out, char *infile, char *command, char **env)
+{
+	int		infile_fd;
+	char	**cmd_args;
+
+	infile_fd = open(infile, O_RDONLY);
+	dup2(infile_fd, STDIN);
+	dup2(fd_out, STDOUT);
+	cmd_args = ft_split(command, ' ');
+	execve(command_path(cmd_args[0], env), cmd_args, env);
+	return (0);
+}
+
+int	middle_behaviour(int fd_in, int fd_out, char *command, char **env)
+{
+	char **cmd_args;
+
+	dup2(fd_in, STDIN);
+	dup2(fd_out, STDOUT);
+	cmd_args = ft_split(command, ' ');
+	execve(command_path(cmd_args[0], env), cmd_args, env);
+	return (0);
+}
+
+int	end_behaviour(int fd_in, char *outfile, char *command, char **env)
+{
+	int		outfile_fd;
+	char	**cmd_args;
+
+	outfile_fd = open(outfile, O_WRONLY | O_TRUNC | O_CREAT, 00644);
+	dup2(outfile_fd, STDOUT);
+	dup2(fd_in, STDIN);
+	cmd_args = ft_split(command, ' ');
+	execve(command_path(cmd_args[0], env), cmd_args, env);
+	return (0);
+}
+
+#define FIRST_CMD 2
+#define ANYPID -1
+
 int	main(int argn, char **argv, char **env)
 {
-	if (argn > 1)
-		printf("%s\n", command_path(argv[1], env));
+	int	current_pipe[2];
+	int fd_in;
+	int	i;
+	int	wstatus;
+
+	if (argn < 5)
+		return (ft_printf("Error: Wrong number of arguments."));
+	pipe(current_pipe);
+	if (fork() == 0)
+		start_behaviour(current_pipe[STDOUT], argv[1], argv[2], env);
+	close(current_pipe[STDOUT]);
+	i = 1;
+	while (argn > 5)
+	{
+		fd_in = current_pipe[STDIN];
+		pipe(current_pipe);
+		if (fork() == 0)
+			middle_behaviour(fd_in, current_pipe[STDOUT], argv[FIRST_CMD + i], env);
+		close(fd_in);
+		close(current_pipe[STDOUT]);
+		i ++;
+		argn --;
+	}
+	if (fork() == 0)
+		end_behaviour(current_pipe[STDIN], argv[FIRST_CMD + i + 1], argv[FIRST_CMD + i], env);
+	close(current_pipe[STDIN]);
+	while (i--)
+		waitpid(ANYPID, &wstatus, 0);
 	return (0);
 }
