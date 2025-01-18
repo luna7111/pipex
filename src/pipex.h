68e20f2457961c2c2f16108d9a230e074f177ca6
Luna <luna7111@tuta.io>
2025-01-18 17:12:02 +0100

    updated header file

--- src/pipex.h
+++ src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/01/10 16:31:46 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/01/15 00:28:44 by ldel-val          ``                     */
+/*   Updated: 2025/01/18 17:01:06 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,13 @@
 # include <stdio.h>
 # include <sys/wait.h>
 # include "../libft/libft.h"
+
+int		start_behaviour(char *infile, char *command, char **env);
+int		here_doc_behaviour(char *delimiter);
+int		middle_behaviour(int fd_in, char *command, char **env);
+void	end_behaviour(int fd_in, char *outfile, char *command, char **env);
+
+void	print_error(char *error);
+char	*command_path(char *name, char **env);
 
 #endif
