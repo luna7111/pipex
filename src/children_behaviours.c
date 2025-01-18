cb91a4bf799787c548cccda912c26a88fbc72008
Luna <luna7111@tuta.io>
2025-01-18 16:32:26 +0100

    added the function for the ending child process

--- src/children_behaviours.c
+++ src/children_behaviours.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/01/18 16:27:58 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/01/18 16:31:21 by ldel-val          ``                     */
+/*   Updated: 2025/01/18 16:32:10 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,4 +52,21 @@ int	middle_behaviour(int fd_in, char *command, char **env)
 	close(fd_in);
 	close(fd[STDOUT]);
 	return (fd[STDIN]);
+}
+
+void	end_behaviour(int fd_in, char *outfile, char *command, char **env)
+{
+	char	**cmd_args;
+	int		fd_out;
+
+	if (fork() == 0)
+	{
+		fd_out = open(outfile, O_WRONLY | O_TRUNC | O_CREAT, 00644);
+		dup2(fd_out, STDOUT);
+		dup2(fd_in, STDIN);
+		cmd_args = ft_split(command, ' ');
+		execve(command_path(cmd_args[0], env), cmd_args, env);
+		perror("end child error: ");
+	}
+	close(fd_in);
 }
