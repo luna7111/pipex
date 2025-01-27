eb5998bec7ab129334d00e90b10c7f0b0e1caac6
ldel-val <ldel-val@student.42madrid.com>
2025-01-27 18:45:16 +0100

    moved file desccriptor check to ensure all fds are closed on failiure

--- src/children_behaviours.c
+++ src/children_behaviours.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/01/18 16:27:58 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/01/27 16:53:28 by ldel-val          ``                     */
+/*   Updated: 2025/01/27 18:43:32 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ int	start_behaviour(char *infile, char *command, char **env)
 	if (fork() == 0)
 	{
 		fd_in = open(infile, O_RDONLY);
-		if (fd_in == -1)
-			perror_exit("");
 		dup2(fd_in, STDIN);
 		dup2(fd[STDOUT], STDOUT);
 		close(fd[STDOUT]);
 		close(fd[STDIN]);
+		if (fd_in == -1)
+			print_error("File does not exist");
 		close(fd_in);
 		cmd_args = ft_split(command, ' ');
 		execve(command_path(cmd_args, env), cmd_args, env);
@@ -49,21 +49,21 @@ int	here_doc_behaviour(char *delimiter)
 		close(fd[STDIN]);
 		ft_printf("pipe heredoc>");
 		read_line = get_next_line(STDIN);
-		write(fd[STDOUT], read_line, ft_strlen(read_line));
 		while (1)
 		{
-			free(read_line);
-			ft_printf("pipe heredoc>");
-			read_line = get_next_line(STDIN);
 			if (ft_strncmp(read_line, delimiter, ft_strlen(delimiter)))
 				write(fd[STDOUT], read_line, ft_strlen(read_line));
 			else
 				break ;
+			free(read_line);
+			ft_printf("pipe heredoc>");
+			read_line = get_next_line(STDIN);
 		}
 		free(read_line);
 		exit(0);
 	}
 	close(fd[STDOUT]);
+	waitpid(ANYPID, NULL, 0);
 	return (fd[STDIN]);
 }
 
@@ -98,12 +98,12 @@ void	end_behaviour(int fd_in, char *outfile, char *command, char **env)
 	if (fork() == 0)
 	{
 		fd_out = open(outfile, O_WRONLY | O_TRUNC | O_CREAT, 00644);
-		if (fd_in == -1)
-			perror_exit("");
 		dup2(fd_out, STDOUT);
 		dup2(fd_in, STDIN);
-		close(fd_in);
 		close(fd_out);
+		if (fd_in == -1)
+			print_error("Cant write to file");
+		close(fd_in);
 		cmd_args = ft_split(command, ' ');
 		execve(command_path(cmd_args, env), cmd_args, env);
 		free_strarray(cmd_args);
